4933ce85fc59cb6a4a56f1735edc42b05349c54f
Hamzautd7 <hamza.utd7@gmail.com>
2018-05-14 06:22:09 +0500

    Initial Commit

---
+++ web-server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+
+hbs.registerPartials(__dirname +'/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return  new Date().getFullYear()+ ' TEST';
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname +'/public'));
+
+app.use((req, res, next) =>{
+    res.render('maintanence.hbs')
+});
+
+
+app.get('/', (req,res)=>{
+    res.render('home.hbs',{
+        pageTitle: 'Home Page!',
+        welcomeMessage: 'Welcome to my website',
+        name: 'Hamza',
+        
+    });
+});
+
+app.get('/about', (req,res)=>{
+    res.render('about.hbs',{
+        pageTitle: 'About Page!',
+        
+    });
+});
+
+app.get('/bad', (req,res)=>{
+    res.send("<h1>Error</h1>");
+});
+
+   app.listen(3000);
